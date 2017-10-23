b977b694e5fe38338ee4589409065a34731a6447
ErnestoSalazar <ernestoalbertosalazar@gmail.com>
2017-10-22 23:44:50 -0700

    Updated Comic Book Detail view to be strongly typed

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ComicBookGallery.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,21 +13,24 @@ namespace ComicBookGallery.Controllers
 
         public ActionResult Detail()
         {
-            // ViewBag is an object in MVC that allow us to pass data from a controller to a view
-
-            ViewBag.SeriesTitle = "The amazing Spiderman";
-            ViewBag.IssueNumber = 700;
-            ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
-            ViewBag.Artists = new string[]
+            var comicBook = new ComicBook()
             {
-                "Script: Dan Slott",
-                "Pencils: Humberto Ramos",
-                "Inks: Victor Olazaba",
-                "Colors: Edgar Delgado",
-                "Letters: Chris Eliopoulos"
+                SeriesTitle = "The amazing Spiderman",
+                IssueNumber = 700,
+                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
+                Artists = new Artist[]
+                {
+                    new Artist(){ Name="Dan Slott", Role = "Script"},
+                    new Artist(){ Name="Humberto Ramos", Role = "Pencils"},
+                    new Artist(){ Name="Victor Olazaba", Role = "Inks"},
+                    new Artist(){ Name="Edgar Delgado", Role = "Colors"},
+                    new Artist(){ Name="Chris Eliopoulos", Role = "Letters"}
+                }
             };
+            // ViewBag is an object in MVC that allow us to pass data from a controller to a view
+            
 
-            return View();
+            return View(comicBook); // we can pass data yo our View method to make it strongly typed
         }
     }
 }
